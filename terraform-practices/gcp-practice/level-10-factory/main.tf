d1f4d3ed1110345ece68f9401540085faf65a88d
asliAdarsh <adarshjaiswal9966@gmail.com>
2026-05-29 23:56:16 +0530

    feat(factory): deploy automated corporate project factory infrastructure

---
+++ terraform-practices/gcp-practice/level-10-factory/main.tf
@@ -0,0 +1,30 @@
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "google_project" "hub_net_core" {
+    name = "hub-net-core"
+    project_id = "hub-net-core-${random_id.suffix.hex}"
+    org_id = var.org_id
+    billing_account = var.billing_account_id
+
+    labels = {
+    tier        = "core"
+    environment = "networking"
+    managed_by  = "terraform"
+  }
+
+}
+
+resource "google_project" "spoke_app_prod" {
+  name            = "spoke-app-prod"
+  project_id      = "spoke-app-prod-${random_id.suffix.hex}"
+  org_id          = "310558825488"
+  billing_account = var.billing_account_id
+
+  labels = {
+    tier        = "application"
+    environment = "production"
+    managed_by  = "terraform"
+  }
+}
---
+++ terraform-practices/gcp-practice/level-10-factory/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  type    = string
+  default = "test-project-18792"
+}
+
+variable "region" {
+  type    = string
+  default = "asia-south1"
+}
+
+variable "org_id"{
+  type    = string
+  default = "310558825488"
+}
+
+variable "billing_account_id" {
+  type        = string
+  description = "The billing account ID linked to your organization"
+}
