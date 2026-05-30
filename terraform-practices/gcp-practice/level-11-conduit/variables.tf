b8b80bfd9f6eed7748088fa74fb34ee4ba81dfff
asliAdarsh <adarshjaiswal9966@gmail.com>
2026-05-30 22:36:44 +0530

    fix(shared-vpc): point host resource to dynamic variable and utilize hub alias

--- terraform-practices/gcp-practice/level-11-conduit/variables.tf
+++ terraform-practices/gcp-practice/level-11-conduit/variables.tf
@@ -1,11 +1,9 @@
 variable "hub_project_id" {
   type    = string
-  default = "test-project-18792"
 }
 
 variable "spoke_project_id" {
   type    = string
-  default = "test-project-18792"
 }
 variable "region" {
   type    = string
