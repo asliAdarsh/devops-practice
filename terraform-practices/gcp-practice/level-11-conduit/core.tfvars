8edd63bace6a260deef030fd99d9be1d50c51d1d
asliAdarsh <adarshjaiswal9966@gmail.com>
2026-05-30 22:39:42 +0530

    fix(conduit): fixed variable issue

--- terraform-practices/gcp-practice/level-11-conduit/core.tfvars
+++
@@ -1,2 +0,0 @@
-hub_project_id   = "hub-net-core-13b75266"
-spoke_project_id = "spoke-app-prod-13b75266"
--- terraform-practices/gcp-practice/level-11-conduit/variables.tf
+++ terraform-practices/gcp-practice/level-11-conduit/variables.tf
@@ -1,9 +1,11 @@
 variable "hub_project_id" {
-  type = string
+  type    = string
+  default = "hub-net-core-13b75266"
 }
 
 variable "spoke_project_id" {
-  type = string
+  type    = string
+  default = "spoke-app-prod-13b75266"
 }
 variable "region" {
   type    = string
